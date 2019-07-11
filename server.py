51fda1669d417b1009aa778a831099d116117956
ZebraStack <50148692+zebrastack@users.noreply.github.com>
2019-07-11 12:32:25 +0100

    Update server.py

--- server.py
+++ server.py
@@ -5,12 +5,10 @@ MESSAGE = "Hello World\n"
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def hello():
     result = MESSAGE.encode("utf-8")
     return result
-
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=PORT)
